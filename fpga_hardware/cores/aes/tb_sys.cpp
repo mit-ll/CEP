dec2e63575db5d73b43584732b7f8e953ea897b3
Matthew Hicks <mdhicks@gmail.com>
2017-04-06 17:11:17 -0400

    Add SoC-level testbench.

---
+++ fpga_hardware/cores/aes/tb_sys.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#include "AES.h"
+
+// Need this to fully implement expected interface
+void runForClockCycles(unsigned int x) {;}
+
+uint32_t readFromAddress(uint32_t pAddress) {
+    return *((uint32_t *)(pAddress + AES_BASE));
+}
+
+void writeToAddress(uint32_t pAddress, uint32_t pData) {
+    *((uint32_t *)(pAddress + AES_BASE)) = pData;
+}
+
+bool ciphertextValid(void) {
+    return (readFromAddress(AES_DONE) != 0) ? true : false;
+}
+
+void start(void) {
+    writeToAddress(AES_START, 0x1);
+    writeToAddress(AES_START, 0x0);
+}
+
+void reportCiphertext(void) {
+    cout << "Ciphertext:\t0x";
+    for(int i = (128 / 32) - 1; i >= 0; --i) {
+        printWordAsBytes(readFromAddress(CT_BASE + i));
+    }
+    cout << endl;
+}
+
+void saveCiphertext(uint32_t *pCT) {
+    for(int i = 0; i < (128 / 32); ++i) {
+        *pCT++ = readFromAddress(CT_BASE + i);
+    }
+}
+
+void setPlaintext(const char* pPT) {
+    cout << "Plaintext:\t0x";
+    for(int i = 0; i < (128 / 8); ++i) {
+        uint32_t temp;
+        for(int j = 0; j < 4; ++j) {
+            ((char *)(&temp))[3 - j] = *pPT++;
+        }
+        writeToAddress(PT_BASE + (3 - i), temp);
+        cout << hex << setfill('0') << setw(8) << temp;
+    }
+    cout << endl;
+}
+
+void setPlaintext(const uint32_t* pPT) {
+    cout << "Plaintext:\t0x";
+    for(int i = 0; i < (128 / 32); ++i) {
+        writeToAddress(PT_BASE + (3 - i), pPT[i]);
+        cout << hex << setfill('0') << setw(8) << pPT[i];
+    }
+    cout << endl;
+}
+
+void setKey(const uint32_t* pKey) {
+    cout << "Key:\t\t0x";
+    for(int i = 0; i < (128 / 32); ++i) {
+        writeToAddress(KEY_BASE + (3 - i), pKey[i]);
+        cout << hex << setfill('0') << setw(8) << pKey[i];
+    }
+    cout << endl;
+}
+
+int main(int argc, char **argv, char **env) {
+    uint32_t ct[128 / 32];
+    
+    uint32_t pt1[4]  = {0x3243f6a8, 0x885a308d, 0x313198a2, 0xe0370734};
+    uint32_t key1[4] = {0x2b7e1516, 0x28aed2a6, 0xabf71588, 0x09cf4f3c};
+    setPlaintext(pt1);
+    setKey(key1);
+    start();
+    waitForValidOutput();
+    reportCiphertext();
+    verifyCiphertext("3925841d02dc09fbdc118597196a0b32", "test 1");
+    
+    
+    
+    uint32_t pt2[4]  = {0x00112233, 0x44556677, 0x8899aabb, 0xccddeeff};
+    uint32_t key2[4] = {0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f};
+    setPlaintext(pt2);
+    setKey(key2);
+    start();
+    waitForValidOutput();
+    reportCiphertext();
+    verifyCiphertext("69c4e0d86a7b0430d8cdb78070b4c55a", "test 2");
+    
+    
+    
+    uint32_t pt3[4]  = {0, 0, 0, 0};
+    uint32_t key3[4] = {0, 0, 0, 0};
+    setPlaintext(pt3);
+    setKey(key3);
+    start();
+    waitForValidOutput();
+    reportCiphertext();
+    verifyCiphertext("66e94bd4ef8a2c3b884cfa59ca342b2e", "test 3");
+    
+    
+    
+    uint32_t pt4[4]  = {0, 0, 0, 0};
+    uint32_t key4[4] = {0, 0, 0, 1};
+    setPlaintext(pt4);
+    setKey(key4);
+    start();
+    waitForValidOutput();
+    reportCiphertext();
+    verifyCiphertext("0545aad56da2a97c3663d1432a3d1c84", "test 4");
+    
+    
+    
+    uint32_t pt5[4]  = {0, 0, 0, 1};
+    uint32_t key5[4] = {0, 0, 0, 0};
+    setPlaintext(pt5);
+    setKey(key5);
+    start();
+    waitForValidOutput();
+    reportCiphertext();
+    verifyCiphertext("58e2fccefa7e3061367f1d57a4e7455a", "test 5");
+    
+    exit(0);
+}

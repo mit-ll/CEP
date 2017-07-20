d9305f667686872165cac47e80f46e5eb64b7dfd
Matthew Hicks <mdhicks@gmail.com>
2017-07-20 13:53:25 -0400

    GPS outputs vectors.

--- fpga_hardware/cores/gps/gps.v
+++ fpga_hardware/cores/gps/gps.v
@@ -4,6 +4,7 @@ module gps(
    sys_clk_in_n,
    sync_rst_in,
    sv_num,
+   startRound,
    ca_code,
    p_code,
    py_code,
@@ -14,8 +15,9 @@ module gps(
    input  sys_clk_in_n;
    input  sync_rst_in;
    input [5:0] sv_num;
-   output ca_code;
-   output p_code;
+   input startRound;
+   output reg [12:0] ca_code;
+   output [127:0] p_code;
    output [127:0] py_code;
    output py_code_valid;
 
@@ -36,39 +38,82 @@ gps_clkgen gps_clk(
 // Combine SoC and clkgen resets
 wire rst_combined = sync_rst_in | gps_clk_rst;
 
+// Control code generators
+reg code_gen_en;
+always @(posedge gps_clk_fast)begin
+   if(rst_combined | startRound)begin
+      code_gen_en <= startRound;
+   end
+   else if(p_bit_count == 8'd128)begin
+      code_gen_en <= 1'b0;
+   end
+end
+
 // Generate C/A code
+wire ca_code_bit;
 cacode ca(
    gps_clk_slow,
    rst_combined,
    sv_num,
-   1'b1,
-   ca_code
+   code_gen_en,
+   ca_code_bit
 );
 
+// Save 13 ca-code bits
+reg [3:0] ca_bit_count;
+always @(posedge gps_clk_slow)begin
+   if(rst_combined | startRound)begin
+      ca_bit_count <= 0;
+      ca_code <= 13'h0;
+   end
+   else begin
+      if(ca_bit_count == 4'd13)begin
+         ;
+      end
+      else begin
+         ca_bit_count <= ca_bit_count + 1;
+         ca_code <= {ca_code[11:0], ca_code_bit};
+      end
+   end
+end
+
 // Generate P-code
+wire p_code_bit;
 pcode p(
     gps_clk_fast,
     rst_combined,
-    1'b1,
+    code_gen_en,
     sv_num,
-    p_code
+    p_code_bit
 );
 
 // Save 128 p-code bits, encrypt and send as py_code
-reg [6:0] p_bit_count;
+reg [7:0] p_bit_count;
 reg [127:0] p_pt;
 always @(posedge gps_clk_fast)begin
-   if(rst_combined)begin
+   if(rst_combined | startRound)begin
       p_bit_count <= 0;
       p_pt <= 128'h0;
    end
    else begin
-      p_bit_count <= p_bit_count + 1;
-      p_pt <= {p_pt [126:0], p_code}; 
+      if(p_bit_count == 8'd128)begin
+         ;
+      end
+      else begin
+         p_bit_count <= p_bit_count + 1;
+         p_pt <= {p_pt [126:0], p_code_bit};
+      end
    end
 end
 
-wire encrypt = p_bit_count == 0;
+assign p_code = p_pt;
+
+reg encrypt;
+always @(posedge gps_clk_fast)begin
+   encrypt <= 0;
+   if(p_bit_count == 8'd127)
+      encrypt <= 1'b1;
+end
 
 aes_192 aes (
    gps_clk_fast,
--- fpga_hardware/cores/gps/gps_test.v
+++ fpga_hardware/cores/gps/gps_test.v
@@ -16,7 +16,9 @@ initial begin
       rst_o <= 0;
 end
 
-wire ca_code, p_code, py_code, py_code_valid;
-gps gps(clk,~clk,rst_o,6'd12,ca_code,p_code,py_code,py_code_valid);
+wire [12:0] ca_code;
+wire [127:0] p_code, py_code;
+wire py_code_valid;
+gps gps(clk,~clk,rst_o,6'd12,1'b1,ca_code,p_code,py_code,py_code_valid);
 
 endmodule
